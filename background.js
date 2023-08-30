f83d9fa9cc8b2caa76261f2ba3781160bf00b59d
piebot3000 <swlemonhead12@gmail.com>
2023-08-30 12:35:16 +0000

    no longer need to be able to write to session storage from content scripts

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-//allow use of session storage in content script
-chrome.storage.session.setAccessLevel({ accessLevel: "TRUSTED_AND_UNTRUSTED_CONTEXTS" });
-
 chrome.runtime.onMessage.addListener((message, sender) => {
     //get the key the data for this tab will be under
     let storageKey = "quizletString" + sender.tab.id;
