ac99baffedd23a41216d052bde3bb399ae393bed
piebot3000 <swlemonhead12@gmail.com>
2023-10-11 16:54:23 +0000

    setup some slightly better logging, deal with wrath cookies, make luckyPrice work on the boost ammount

--- content_scripts/cookieclicker.js
+++ content_scripts/cookieclicker.js
@@ -1,8 +1,9 @@
 var cookin = {
   ints: [],
-  
+  clickWrath: false,
+
   luckyPrice: function() {
-    return Game.cookiesPs * 6000;
+    return Game.cookiesPsRaw * 7 * 6000;
   },
 
   getBestValue: function() {
@@ -18,27 +19,60 @@ var cookin = {
   startAutoBuy: function() {
     var int = setInterval(() => {
       if (Game.cookies > this.luckyPrice()) {
-        this.getBestValue().buy();
+        var best = this.getBestValue();
+        if (Game.cookies > best.price) {
+          best.buy();
+          this.logger.log("AutoBought: " + best.name);
+        }
       }
-    }, 1000);
+    }, 10000);
 
     this.ints.push(int);
   },
 
   startAutoGold: function() {
     var int = setInterval(() => {
-      Game.shimmers.forEach(function(s) { s.pop(); })
+      Game.shimmers.forEach((s) => { 
+        if(shimmer.type == "golden" && shimmer.wrath == 0) {
+          s.pop(); 
+          this.logger.log("Popped Golden Cookie"); 
+        } else {
+          if(this.clickWrath) {
+            s.pop();
+            this.logger.log("Popped Wrath Cookie");
+          } else {
+            this.logger.log("Skipped Wrath Cookie");
+          }
+        }
+      });
     }, 1000);
 
     this.ints.push(int);
   },
 
   clearIntervals: function() {
-    this.ints.forEach((i) => {clearInterval(i)});
+    this.ints.forEach((i) => { clearInterval(i) });
     this.ints = [];
   },
 
   luckyPriceDiff: function() {
     return Beautify(this.luckyPrice() - Game.cookies);
+  }
+}
+
+cookin.logger = {
+  printlogs: false,
+  logs: [],
+  log: function(text) {
+    if (this.printlogs) {
+      console.log(text);
+    }
+    this.logs.push(text);
+  },
+  showLogs: function() {
+    console.log(this.logs);
+  },
+  clearLogs: function() {
+    this.logs = [];
   }
 }
